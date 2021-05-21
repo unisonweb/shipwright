4216f7d1b5f4fbc2380f744754f8da582856594d
Stew O'Connor <stew@vireo.org>
2021-05-21 12:51:53 -0700

    only use the home codebase

--- files/initialize-codebase.sh
+++ files/initialize-codebase.sh
@@ -8,13 +8,26 @@ git init --bare /tmp/codebase
 
 rm -rf $(HOME)/.cache/unisonlanguage
 CODEBASE=$(/usr/local/bin/ucm transcript -save-codebase /usr/local/share/share.transcript.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
-rm -rf $(HOME)/.cache/unisonlanguage
-CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase /usr/local/share/push.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
-rm -rf $(HOME)/.cache/unisonlanguage
-CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase -codebase $CODEBASE --old-codebase /usr/local/share/share.transcript.old.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
-rm -rf $(HOME)/.cache/unisonlanguage
-CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase /usr/local/share/push.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
-rm -rf $(HOME)/.cache/unisonlanguage
-CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase -codebase $CODEBASE /usr/local/share/pull.md | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
 
-mv $CODEBASE/.unison /root/.unison
+rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
+mv $CODEBASE/.unison $(HOME)/.unison
+
+CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase /usr/local/share/push.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
+
+rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
+mv $CODEBASE/.unison $(HOME)/.unison
+
+CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase --old-codebase /usr/local/share/share.transcript.old.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
+
+rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
+mv $CODEBASE/.unison $(HOME)/.unison
+
+CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase /usr/local/share/push.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
+
+rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
+mv $CODEBASE/.unison $(HOME)/.unison
+
+CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase /usr/local/share/pull.md | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
+
+rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
+mv $CODEBASE/.unison $(HOME)/.unison
