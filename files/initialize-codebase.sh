fce9cedc69201d2755363816133670ec61c57fa8
Stew O'Connor <stew@vireo.org>
2021-05-21 12:53:47 -0700

    duh

--- files/initialize-codebase.sh
+++ files/initialize-codebase.sh
@@ -6,28 +6,28 @@ rm -fr /tmp/codebase
 mkdir /tmp/codebase
 git init --bare /tmp/codebase
 
-rm -rf $(HOME)/.cache/unisonlanguage
+rm -rf ${HOME}/.cache/unisonlanguage
 CODEBASE=$(/usr/local/bin/ucm transcript -save-codebase /usr/local/share/share.transcript.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
 
-rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
-mv $CODEBASE/.unison $(HOME)/.unison
+rm -rf ${HOME}/.unison ${HOME}/.cache/unisonlanguage
+mv $CODEBASE/.unison ${HOME}/.unison
 
 CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase /usr/local/share/push.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
 
-rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
-mv $CODEBASE/.unison $(HOME)/.unison
+rm -rf ${HOME}/.unison ${HOME}/.cache/unisonlanguage
+mv $CODEBASE/.unison ${HOME}/.unison
 
 CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase --old-codebase /usr/local/share/share.transcript.old.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
 
-rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
-mv $CODEBASE/.unison $(HOME)/.unison
+rm -rf ${HOME}/.unison ${HOME}/.cache/unisonlanguage
+mv $CODEBASE/.unison ${HOME}/.unison
 
 CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase /usr/local/share/push.md  | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
 
-rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
-mv $CODEBASE/.unison $(HOME)/.unison
+rm -rf ${HOME}/.unison ${HOME}/.cache/unisonlanguage
+mv $CODEBASE/.unison ${HOME}/.unison
 
 CODEBASE=$(/usr/local/bin/ucm transcript.fork -save-codebase /usr/local/share/pull.md | awk '/You can run/{sub(".*-codebase ", "", $0); sub("`.*", "", $0) ; print}')
 
-rm -rf $(HOME)/.unison $(HOME)/.cache/unisonlanguage
-mv $CODEBASE/.unison $(HOME)/.unison
+rm -rf ${HOME}/.unison ${HOME}/.cache/unisonlanguage
+mv $CODEBASE/.unison ${HOME}/.unison
