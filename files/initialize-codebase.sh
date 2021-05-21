1a4946863a1f677b40688bb4da372744c1e894b6
Stew O'Connor <stew@vireo.org>
2021-05-21 12:10:16 -0700

    clear the cache

--- files/initialize-codebase.sh
+++ files/initialize-codebase.sh
@@ -3,9 +3,9 @@
 set -ex
 
 rm -fr /tmp/codebase
-
+rm -rf $(HOME)/.cache/unisonlanguage
 mkdir /tmp/codebase
-git init /tmp/codebase
+git init --bare /tmp/codebase
 
 /usr/local/bin/ucm transcript /usr/local/share/share.transcript.md
 /usr/local/bin/ucm transcript --old-codebase /usr/local/share/share.transcript.old.md
